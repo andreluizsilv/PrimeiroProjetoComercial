bd42f24d3c4d0bb2ae576a523db52ba76daf0469
andreluizsilv <andre_luiz29@hotmail.com>
2023-04-16 13:14:11 -0300

    Criando Class

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import homepage, homecadastro
+from .views import Homecadastro, Homepage
 
 urlpatterns = [
-    path('', homepage),
-    path('cliente_cadastrado/', homecadastro),
+    path('', Homepage.as_view()),
+    path('cliente_cadastrado/', Homecadastro.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Cliente
+from django.views.generic import TemplateView, ListView
+
+
 # Create your views here.
-def homepage(request):
-    return render(request, "homepage.html")
+#def homepage(request):
+    #return render(request, "homepage.html")
 
 # view - url html
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+#def homecadastro(request):
+#    context = {}
+#    clientes_cadastrado = Cliente.objects.all()
+#    context['clientes_cadastrado'] = clientes_cadastrado
+#    return render(request, "homecadastro.html", context)
 
-def homecadastro(request):
-    context = {}
-    clientes_cadastrado = Cliente.objects.all()
-    context['clientes_cadastrado'] = clientes_cadastrado
-    return render(request, "homecadastro.html", context)
+class Homecadastro(ListView):
+    template_name = "homecadastro.html"
+    model = Cliente
